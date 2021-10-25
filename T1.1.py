fe22115e965dbbedb4966209f0d546a263d6c780
Jo-B-314 <s8jobech@stud.uni-saarland.de>
2021-10-25 10:26:28 +0200

    typo

--- T1.1.py
+++ T1.1.py
@@ -5,7 +5,7 @@ k1 = 4; k2 = 6; k3 = 20
 counts = Counter()
 total = k1 * k2 * k3
 
-for oneresult in product(range(1,k1+1), range(1, k2+1),range(1, 3+1)):
+for oneresult in product(range(1,k1+1), range(1, k2+1),range(1, k3+1)):
     value = max(0, sum(oneresult))
     counts[value] += 1
 outcomes = sorted(counts.keys())
