dd63841acc68f09f6659147ae2e006d87f80de37
Jo-B-314 <s8jobech@stud.uni-saarland.de>
2021-10-26 13:06:07 +0200

    T1.4 with floor division

--- T1.4.py
+++ T1.4.py
@@ -8,26 +8,31 @@ def ntuples(n,k) :
 def npermutations(n,k) :
     a = factorial(n)
     b = factorial (n-k)
-    return int(a/b)
+    # a / b leads to 'OverflowError: integer division result too large for a float'
+    return int(a // b)
 
 def ncombinations(n,k) :
     a = factorial(n)
     b = factorial(n-k)
     c = factorial (k)
-    return int(a / (b * c))
+    return int(a // (b * c))
 
 def nmultisets(n,k) :
     a = factorial(n+k-1)
     b = factorial(n-1)
     c = factorial(k)
-    return int(a / (b * c))
+    return int(a // (b * c))
 
 print("Enter n:")
 n = int(input())
 print("Enter k:")
 k = int(input())
 
+print()
 print(ntuples(n,k))
+print()
 print(npermutations(n,k))
+print()
 print(ncombinations(n,k))
+print()
 print(nmultisets(n,k))
