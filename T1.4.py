0f3042675203bdb386c4374429f5abbb40634c2e
Jo-B-314 <s8jobech@stud.uni-saarland.de>
2021-10-27 09:52:46 +0200

    fraction is your friend

--- T1.4.py
+++ T1.4.py
@@ -1,6 +1,6 @@
 from itertools import product
 from math import factorial
-from collections import Counter
+from fractions import Fraction as frac
 
 def ntuples(n,k) :
     return n**k
@@ -9,19 +9,20 @@ def npermutations(n,k) :
     a = factorial(n)
     b = factorial (n-k)
     # a / b leads to 'OverflowError: integer division result too large for a float'
-    return int(a // b)
+    c = frac(a,b)
+    return c
 
 def ncombinations(n,k) :
     a = factorial(n)
     b = factorial(n-k)
     c = factorial (k)
-    return int(a // (b * c))
+    return int(a / (b * c))
 
 def nmultisets(n,k) :
     a = factorial(n+k-1)
     b = factorial(n-1)
     c = factorial(k)
-    return int(a // (b * c))
+    return int(a / (b * c))
 
 print("Enter n:")
 n = int(input())
